cb4506063720833c399717618589bafd926d1e1d
CJoltun <matiusshaunpatrickhendra@gmail.com>
2023-11-22 17:59:36 +0700

    Second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/CJolun"
+                href="https://twitter.com/CJoltun"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
